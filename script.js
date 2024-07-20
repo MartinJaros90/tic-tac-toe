97a193b07bfd6b62759d2d1d11832c984014235b
Martin Jaros <martinjaros88@gmail.com>
2024-07-20 22:43:22 +0200

    Cross and circle animation

--- script.js
+++ script.js
@@ -27,9 +27,9 @@ function render() {
             const index = i * 3 + j;
             let symbol = '';
             if (fields[index] === 'circle') {
-                symbol = 'o';
+                symbol = generateCircleSVG();
             } else if (fields[index] === 'cross') {
-                symbol = 'x';
+                symbol = generateXCrossSVG();
             }
             tableHTML += `<td>${symbol}</td>`;
         }
@@ -40,3 +40,59 @@ function render() {
     contentDiv.innerHTML = tableHTML;
 }
     
+
+function generateCircleSVG() {
+    // Radius of the circle
+    const radius = 30;
+    // Circumference of the circle
+    const circumference = 2 * Math.PI * radius;
+
+    // Return the SVG string with embedded CSS for animation
+    return `
+<svg width="70" height="70" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
+    <style>
+        .circle {
+            stroke-dasharray: ${circumference};
+            stroke-dashoffset: ${circumference};
+            animation: fillCircle 1s forwards;
+        }
+        @keyframes fillCircle {
+            to {
+                stroke-dashoffset: 0;
+            }
+        }
+    </style>
+    <circle cx="35" cy="35" r="${radius}" stroke="#00B0EF" stroke-width="5" fill="none" class="circle"/>
+</svg>`;
+}
+
+
+function generateXCrossSVG() {
+    // Length of the diagonal line
+    const diagonalLength = Math.sqrt(Math.pow(70, 2) + Math.pow(70, 2));
+    
+    // Return the SVG string with embedded CSS for animation
+    return `
+<svg width="70" height="70" viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
+    <style>
+        .line {
+            stroke-dasharray: ${diagonalLength};
+            stroke-dashoffset: ${diagonalLength};
+            animation: drawLine 2s forwards;
+        }
+        @keyframes drawLine {
+            to {
+                stroke-dashoffset: 0;
+            }
+        }
+    </style>
+    <line x1="15" y1="15" x2="55" y2="55" stroke="#FFC000" stroke-width="5" class="line"/>
+    <line x1="55" y1="15" x2="15" y2="55" stroke="#FFC000" stroke-width="5" class="line"/>
+</svg>`;
+}
+
+
+
+
+
+
